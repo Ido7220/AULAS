2c5852d42ed9e0b1c8475eb6b69fec4ddf2069ae
Renan Ido <iddo.renan@gmail.com>
2025-03-17 21:13:34 -0300

    atualizacao3

---
+++ SEMANA 6/script.js
@@ -0,0 +1,29 @@
+
+console.log("HEllo world");
+//comentario
+//var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10; //obrigado a dar um valor
+
+primeiroTipo = 2;
+segundoTipo = 3;
+
+console.log("primeiroTipo + segundoTipo");
+console.log("segundoTipo + terceiroTipo");
+
+"joao" + 2;
+
+var nome;
+var idade;
+
+nome = "joao";
+idade= "25";
+console.log(nome);
+nome + idade;
+console.log(idade);
+
+
+function hello(){
+    document.getElementById("Hello").innerHTML = 
+                                    "Hello World" //
+}
